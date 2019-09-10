6a76c1434b675c71b7ee2891e72ba8c8e843f8be
232524z <ianhaile@me.com>
2019-09-10 18:11:00 -0400

    added comments

--- homework/windchill.py
+++ homework/windchill.py
@@ -1,3 +1,11 @@
+"""
+Ian
+9/10/19
+this program calculates the wind chill for a given
+temperature and wind speed.
+sources: worksheet with formula
+"""
+
 t = float(input("t="))
 v = float(input("v="))
 windchill = int((35.74)+(0.6215*t)+((0.4275*t)-35.75)*(v**.16))
