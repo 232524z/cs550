7e0a52d641165edecbf6dec767bbeda95235b565
232524z <ianhaile@me.com>
2019-09-14 16:56:28 -0400

    Update gradecalc.py

--- homework/gradecalc.py
+++ homework/gradecalc.py
@@ -6,14 +6,10 @@ Sources: https://www.geeksforgeeks.org/python-list/
 https://www.jquery-az.com/5-examples-get-python-list-length-array-tuple-dictionary-also/
 https://wiki.python.org/moin/ForLoop
 OMH
-
-
-
 '''
 gradeNumbers = [0, 1, 1.5, 2, 2.5, 2.85, 3.2, 3.5, 3.85, 4.2, 4.5, 4.65, 4.85]
 gradeLetters = ['F','D-','D','D+','C-','C','C+','B-','B','B+','A-','A','A+']
 score = float(input("Score (0-5)\n"))
-
 if score < 0 or score > 5:
 	print("Error! Score must be between 0 and 5!")
 	exit()
