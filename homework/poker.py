368349c20b32bce5672733d642c2a59e0c0a64e7
232524z <ianhaile@me.com>
2019-09-15 22:16:13 -0400

    Update poker.py

--- homework/poker.py
+++ homework/poker.py
@@ -6,58 +6,77 @@ https://snakify.org/en/lessons/two_dimensional_lists_arrays/
 
 '''
 import random
-#creates arrays of cards
-cards = []
+deck = []
+#creates arrays of deck = []
 playerHand = []
 aiHand = []
-#creates cards
-suits = ['h','s','d','c']
+publicCards = []
+#creates deck
+suits = [' of hearts',' of spades',' of diamonds',' of clubs']
 for s in suits:
 	for i in range(2, 15):
-		cards.append(str(i)+s)
+		deck.append(str(i)+s)
 #gets number of opponents
 opponentsNumber = int(input('how many opponents?\n'))
 
 
 
 def shuffle():
-	random.shuffle(cards)
+	random.shuffle(deck)
 
 def collectCards():
-	#adds players cards to the back of the deck
-	cards.append(playerHand[0])
-	cards.append(playerHand[1])
+	for i in range(0,5):
+		deck.append(publicCards[i])
+	publicCards.clear()
+	#adds players deck to the back of the deck
+	deck.append(playerHand[0])
+	deck.append(playerHand[1])
 	#removes all cards from player's hand
 	playerHand.clear()
 	for i in range(0, opponentsNumber):
 		#adds the cards from the ai's hand to the end of the deck
-		cards.append(aiHand[i][0])
-		cards.append(aiHand[i][1])
+		deck.append(aiHand[i][0])
+		deck.append(aiHand[i][1])
 	aiHand.clear()
 
 def dealHand():
 	#deals cards to the player and ai
-	playerHand.append(cards[0])
-	cards.pop(0)
-	playerHand.append(cards[0])
-	cards.pop(0)
+	playerHand.append(deck[0])
+	deck.pop(0)
+	playerHand.append(deck[0])
+	deck.pop(0)
 	for i in range(0, opponentsNumber):
-		aiHand.append([cards[0],cards[1]])
-		cards.pop(0)
-		cards.pop(0)
+		aiHand.append([deck[0],deck[1]])
+		deck.pop(0)
+		deck.pop(0)
+def deal3():
+	#burn 1
+	deck.append(deck[0])
+	deck.pop(0)
+	#deal 3
+	for i in range(0,3):
+		publicCards.append(deck[0])
+		deck.pop(0)
 
+def deal1():
+	#burn 1
+	deck.append(deck[0])
+	deck.pop(0)
+	#deal 1
+	publicCards.append(deck[0])
+	deck.pop(0)
+
+def displayCards():
+	print("\nYour Hand\n",playerHand)
+	print("\n\nTable\n",publicCards)
 
 
 shuffle()
 dealHand()
-print(playerHand)
-print(aiHand)
-collectCards()
-shuffle()
-dealHand()
-print(playerHand)
-print(aiHand)
-
+deal3()
+deal1()
+deal1()
+displayCards()
 
 
 
