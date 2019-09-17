7848d25693b287cc38cb54978d73a9e0bf06882d
232524z <ianhaile@me.com>
2019-09-17 08:55:10 -0400

    Update Number_guessing_game.py

--- in class/Number_guessing_game.py
+++ in class/Number_guessing_game.py
@@ -3,9 +3,12 @@ import random
 thenumber = random.randint(1,100)
 print("Im thinking of a number between 1 and 100\n")
 def numberGame():
+	tries = 10
 	while(True):
+		if tries < 0:
+			print("Oh no! You ran out of tries!")
+			break
 		userguess = input("Guess >>>")
-
 		try:
 			userguess = int(userguess)
 			if userguess == thenumber:
@@ -13,8 +16,10 @@ def numberGame():
 				break
 			elif userguess > thenumber:
 				print("The number is LESS than",userguess)
+				tries-=1
 			elif userguess < thenumber:
 				print("The number is GREATER than ",userguess)
+				tries-=1
 		except ValueError:
 			print('Please enter integer!')
 
