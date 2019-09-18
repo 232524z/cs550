18ba2bc51486b0660b7ee24f394a56e8fd7bbbb7
232524z <ianhaile@me.com>
2019-09-18 14:19:20 -0400

    Finished working on class Opponent

--- homework/poker.py
+++ homework/poker.py
@@ -14,57 +14,85 @@ class Opponent:
 		self.money = 1500
 		self.bluff = False
 		self.handQuality = 0
+		self.willBet = 0
 
+	def bet(self):
+		if Bluff == True:
+			willBet = money*(1/2)
+		if handQuality < 150:
+			willBet = money*(1/20)
+		elif handQuality>2000:
+			willBet = money*(3/4)
+		else:
+			willBet = money*(handQuality/3000)
 
 	def give(self, card):
 		self.hand.append(card)
+
 	def lose(self, ammount):
 		self.money -= ammount
+
 	def gain(self, ammount):
 		self.money += amount
-	def calcQh(self):
-		#gets the number of the card without suite
+
+	def calcQ(self, Round):
+		if Round == 1 and random.int(0,100)<10:
+			bluff = True
 		card1value = int(self.hand[0].split()[0])
 		card1suite = self.hand[0].split(str(card1value))[1]
 		card2value = int(self.hand[1].split()[0])
 		card2suite = self.hand[1].split(str(card2value))[1]
 		#makes a starting hand quality value based on numbers
-		handQuality = card2value+card1value
-		#each potentially good quality doubles hand quality e.g. pair, potential straight, potential flush etc.
-		if abs(card1value-card2value) <=4:
+		if card1value>card2value:
+			handQuality = card1value
+		else: handQuality = card2value
+		#each potentially good quality increases hand quality e.g. pair, potential straight, potential flush etc.
+		if abs(card1value-card2value) <=3:
 			handQuality*=2
 		if card1value == card2value:
-			handQuality*=2
+			handQuality*=8
 		#compaiers suits of cards
 		if card1suite == card2suite:
 			handQuality*=2
+		#if there are cards on the table
+		if Round >= 3:
+			for i in range(0,3):
+				#potential straight
+				if abs(card1value-publicCardsValue[i])<3 and abs(card1value-card2value)<3:
+					handQuality*=2
+				if abs(card2value-publicCardsValue[i])<3 and abs(card2value-card2value)<3:
+					handQuality*=2
+				#potential flush
+				if publicCardsSuite[i]==card1suite or publicCardsSuite[i]==card2suite:
+					handQuality*=2
+				#pair/three of a kind
+				if publicCardsValue[i]==card1suite or publicCardsValue[i]==card2value:
+					handQuality*=8
+		if Round >= 4:
+			#potential straight
+			if abs(card1value-publicCardsValue[3])<3 and abs(card1value-card2value)<3:
+					handQuality*=2
+			if abs(card2value-publicCardsValue[3])<3 and abs(card2value-card2value)<3:
+					handQuality*=2
+			#potential flush
+			if publicCardsSuite[3]==card1suite or publicCardsSuite[i]==card2suite:
+					handQuality*=2
+			#pair/three of a kind/four of a kind/full house
+			if publicCardsValue[3]==card1suite or publicCardsValue[i]==card2value:
+					handQuality*=8
+		if Round == 5:
+			#potential straight
+			if abs(card1value-publicCardsValue[4])<3 and abs(card1value-card2value)<3:
+					handQuality*=2
+			if abs(card2value-publicCardsValue[4])<3 and abs(card2value-card2value)<3:
+					handQuality*=2
+			#potential flush
+			if publicCardsSuite[4]==card1suite or publicCardsSuite[i]==card2suite:
+					handQuality*=2
+			#pair/three of a kind/four of a kind/full house
+			if publicCardsValue[4]==card1suite or publicCardsValue[i]==card2value:
+					handQuality*=8
 		print(handQuality)
-	def calcQ3(self):
-		card1value = int(self.hand[0].split()[0])
-		card1suite = self.hand[0].split(str(card1value))[1]
-		card2value = int(self.hand[1].split()[0])
-		card2suite = self.hand[1].split(str(card2value))[1]
-		#makes a starting hand quality value based on numbers
-		handQuality = card2value+card1value
-		#each potentially good quality doubles hand quality e.g. pair, potential straight, potential flush etc.
-		if abs(card1value-card2value) <=4:
-			handQuality*=2
-		if card1value == card2value:
-			handQuality*=2
-		#compaiers suits of cards
-		if card1suite == card2suite:
-			handQuality*=2
-		for i in range(0,3):
-			if abs(card1value-publicCardsValue[i])<3:
-				handQuality*=2
-			if publicCardsSuite[i]==card1suite or publicCardsSuite[i]==card2suite:
-				handQuality*=2
-		print(handQuality)
-
-
-
-
-
 
 class Player:
 	money = 1500
@@ -72,7 +100,6 @@ class Player:
 		self.hand = []
 	def give(self, card):
 		self.hand.append(card)
-
 
 #creates array of AIs
 ai = []
@@ -86,6 +113,7 @@ publicCards = []
 publicCardsValue = []
 publicCardsSuite = []
 
+amountRaised = 0
 
 #creates deck
 suits = [' of hearts',' of spades',' of diamonds',' of clubs']
@@ -108,12 +136,8 @@ for i in range(0,playerNumber):
 	#adds opponets to the ai array
 	ai.append(Opponent())
 	
-
-
-
 def shuffle():
 	random.shuffle(deck)
-
 
 def collectCards():
 	#adds public cards to the end of the deck
@@ -130,9 +154,7 @@ def collectCards():
 	publicCardsSuite.clear()
 	publicCardsValue.clear()
 
-
 def dealHand():
-	
 	#deals cards to the player
 	player.give(deck[0])
 	player.give(deck[1])
@@ -146,9 +168,6 @@ def dealHand():
 		deck.pop(0)
 		deck.pop(0)
 
-
-
-
 def deal3():
 	#burn 1
 	deck.append(deck[0])
@@ -160,25 +179,24 @@ def deal3():
 		publicCardsSuite.append(deck[0].split(str(publicCardsValue[i]))[1])
 		deck.pop(0)
 
-
-def deal1():
+def deal1(Round):
 	#burn 1
 	deck.append(deck[0])
 	deck.pop(0)
 	#deal 1
 	publicCards.append(deck[0])
 	publicCardsValue.append(int(deck[0].split()[0]))
-	publicCardsSuite.append(deck[0].split(str(publicCardsValue[i]))[1])
+	publicCardsSuite.append(deck[0].split(str(publicCardsValue[Round]))[1])
 	deck.pop(0)
 
 def displayCards():
 	print("\nYour Hand\n",player.hand)
 	print("\n\nTable\n",publicCards)
 
-
 def showOptions():
 	while True:
-		choice = input("Fold	","Check	","Call",amountRaised,"		Raise").strip().lower()
+		print("\nFold		Check		Call",amountRaised,"		Raise")
+		choice = (input("\n")).lower().strip()
 		if choice == "fold":
 			fold()
 			break
@@ -207,36 +225,7 @@ def Raise():
 '''
 def gameplay():
 	shuffle()
-
-shuffle()
-dealHand()
-deal3()
-print(publicCards)
-for i in range(0, playerNumber):
-	ai[i].calcQh()
-	ai[i].calcQ3()
-	print(ai[i].hand)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	dealHand()
+	displayCards()
+	showOptions()
+gameplay()
