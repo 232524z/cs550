bb17bd9df860ec9512fec3265db81343fd0fff73
232524z <ianhaile@me.com>
2019-09-29 17:43:59 -0400

    finished gameplay of poker

--- homework/poker.py
+++ homework/poker.py
@@ -14,58 +14,54 @@ class Opponent:
 		self.hand = []
 		self.handNumbers =[]
 		self.money = 1500
-		self.bluff = False
 		self.handQuality = 0
 		self.willBet = 0
 		self.inRound = True
 		self.pos = pos
-		self.amountSpent = 0
 #determines how much to bet
 	def betCalc(self):
 		global tableMoney
 		global amountRaised
-		if self.bluff == True:
-			self.willBet = (random.randint(0,round(self.money/5))+self.money)*(1/2)
-		else:
-			if len(publicCards) == 0:
-				if self.handQuality < 5:
-					self.willBet = 0
-					self.inRound = False
-				elif self.handQuality>100:
-					self.willBet = (random.randint(0,round(self.money/5))+self.money)*(3/4)
-				else:
-					self.willBet = self.money*(self.handQuality/3000)
-
-			if len(publicCards) == 5:
-				if self.handQuality < 150:
-					self.willBet = 0
-					self.inRound = False
-				elif self.handQuality>2000:
-					self.willBet = (random.randint(0,(self.money/5))+self.money)*(3/4)
-				else:
-					self.willBet = self.money*(self.handQuality/3000)
-
+		if len(publicCards) == 0:
+			self.willBet = 5*(random.randint(0,10))+self.handQuality
+		elif len(publicCards) == 3:
+			self.willBet = 15*(random.randint(0,10))+5*self.handQuality
+		elif len(publicCards) >=4:
+			self.willBet =15*(random.randint(0,10))+10*self.handQuality
+		while(self.willBet>self.money):
+			self.willBet*(3/4)
 
 	def bet(self, name):
+		global playersFolded
 		global amountRaised
 		global playersChecked
 		global userRaised
-		betThisRound = self.willBet*(random.randint(8,12))/10
 		if self.inRound == True:
-			if amountRaised==0 and self.amountSpent == 0:
-				self.lose((3/4)*betThisRound)
-				amountRaised = round((3/4)*betThisRound)
-				print("Player",name,"raised",amountRaised)
+			if amountRaised==0:
+				self.lose((3/4)*self.willBet)
+				amountRaised = round((3/4)*self.willBet)
+				print("Player",name+1,"raised",amountRaised)
 				playersChecked = 0
 				playerRaised = self.pos
 				userRaised = False
-			elif amountRaised < self.willBet and amountRaised != 0:
-				self.lose(amountRaised)
-				print("Player",name,"called")
-				playersChecked+=1
+			elif amountRaised < self.willBet:
+				if self.willBet/1.3 > amountRaised:
+					self.lose((3/4)*self.willBet)
+					amountRaised += (round((3/4)*self.willBet)-amountRaised)
+					print("Player",name+1,"raised",amountRaised)
+					playersChecked = 0
+					playerRaised = self.pos
+					userRaised = False
+				else:
+					self.lose(amountRaised)
+					print("Player",name+1,"called")
+					playersChecked+=1
 			elif self.inRound == True:
 				self.inRound = False
-				print("Player",name,"folded")
+				print("Player",name+1,"folded")
+				playersFolded += 1
+		if self.handQuality > 2000:
+			self.handQuality = 2000
 
 	def give(self, card):
 		self.hand.append(card)
@@ -75,15 +71,12 @@ class Opponent:
 		self.money -= amount
 		global tableMoney
 		tableMoney += amount
-		self.amountSpent += amount
 
 	def gain(self, amount):
 		amount = round(amount)
 		self.money += amount
 
 	def calcQ(self, Round):
-		if Round == 1 and random.randint(0,100)<10:
-			self.bluff = True
 		card1value = int(self.hand[0].split()[0])
 		card1suite = self.hand[0].split(str(card1value))[1]
 		card2value = int(self.hand[1].split()[0])
@@ -143,9 +136,68 @@ class Player:
 	money = 1500
 	def __init__(self):
 		self.hand = []
-		self.amountSpent = 0
+		self.handNumbers =[]
+		self.money = 1500
+		self.handQuality = 0
 	def give(self, card):
 		self.hand.append(card)
+
+	def calcQ(self):
+		Round = 5
+		card1value = int(self.hand[0].split()[0])
+		card1suite = self.hand[0].split(str(card1value))[1]
+		card2value = int(self.hand[1].split()[0])
+		card2suite = self.hand[1].split(str(card2value))[1]
+		#makes a starting hand quality value based on numbers
+		if card1value>card2value:
+			self.handQuality = card1value
+		else: self.handQuality = card2value
+		#each potentially good quality increases hand quality e.g. pair, potential straight, potential flush etc.
+		if abs(card1value-card2value) <=3:
+			self.handQuality*=2
+		if card1value == card2value:
+			self.handQuality*=8
+		#compaiers suits of cards
+		if card1suite == card2suite:
+			self.handQuality*=2
+		#if there are cards on the table
+		if Round >= 3:
+			for i in range(0,3):
+				#potential straight
+				if abs(card1value-publicCardsValue[i])<3 and abs(card1value-card2value)<3:
+					self.handQuality*=2
+				if abs(card2value-publicCardsValue[i])<3 and abs(card2value-card2value)<3:
+					self.handQuality*=2
+				#potential flush
+				if publicCardsSuite[i]==card1suite or publicCardsSuite[i]==card2suite:
+					self.handQuality*=2
+				#pair/three of a kind
+				if publicCardsValue[i]==card1suite or publicCardsValue[i]==card2value:
+					self.handQuality*=8
+		if Round >= 4:
+			#potential straight
+			if abs(card1value-publicCardsValue[3])<3 and abs(card1value-card2value)<3:
+					self.handQuality*=2
+			if abs(card2value-publicCardsValue[3])<3 and abs(card2value-card2value)<3:
+					self.handQuality*=2
+			#potential flush
+			if publicCardsSuite[3]==card1suite or publicCardsSuite[i]==card2suite:
+					self.handQuality*=2
+			#pair/three of a kind/four of a kind/full house
+			if publicCardsValue[3]==card1suite or publicCardsValue[i]==card2value:
+					self.handQuality*=8
+		if Round == 5:
+			#potential straight
+			if abs(card1value-publicCardsValue[4])<3 and abs(card1value-card2value)<3:
+					self.handQuality*=2
+			if abs(card2value-publicCardsValue[4])<3 and abs(card2value-card2value)<3:
+					self.handQuality*=2
+			#potential flush
+			if publicCardsSuite[4]==card1suite or publicCardsSuite[i]==card2suite:
+					self.handQuality*=2
+			#pair/three of a kind/four of a kind/full house
+			if publicCardsValue[4]==card1suite or publicCardsValue[i]==card2value:
+					self.handQuality*=8
 
 #creates array of AIs
 ai = []
@@ -163,6 +215,7 @@ playerNumber = 0
 playerRaised = 0
 playersChecked = 0
 userRaised = False
+playersFolded = 0
 #creates deck
 suits = [' of hearts',' of spades',' of diamonds',' of clubs']
 for s in suits:
@@ -185,12 +238,15 @@ def startup():
 		global playerNumber
 		try:
 			playerNumber = int(input('how many opponents?\n'))
-			break
+			if playerNumber > 0 and playerNumber <= 22:
+				break
+			else: print("Please enter a number between 1 and 22!")
 		except ValueError:
-			print("Please enter an integer!")
+			print("Please enter an integer")
 	for i in range(playerNumber):
 	#adds opponets to the ai array
 		ai.append(Opponent(i))
+	print("\n\n\n\n")
 
 #shuffles deck
 def shuffle():
@@ -265,6 +321,10 @@ def highHand():
 def displayCards():
 	print("\nYour Hand\n",player.hand)
 	print("\n\nTable\n",publicCards)
+	print("\nMoney:",player.money)
+	for i in range(playerNumber):
+		print("\nPlayer",(i+1),"'s money:",ai[i].money)
+	print("\n")
 
 #shows player what they can do
 def showOptions():
@@ -299,52 +359,105 @@ def fold():
 def call(): 
 	global tableMoney
 	global amountRaised
-	playersChecked = 0
+	global playersChecked
 	player.money-=amountRaised
 	tableMoney+=amountRaised
-	player.amountSpent = amountRaised
 	playersChecked +=1
-	amountRaised=0
 
 def Raise():
 	global tableMoney
 	global amountRaised
 	global playersChecked
 	response = int(input("How much do you want to raise?\n"))
-	amountRaised = response
-	playersChecked = 0
-	player.money -= response
-	tableMoney += response
-	userRaised = True
+	if response<amountRaised:
+		print("Not enough! Automatically checking")
+	else:
+		amountRaised = response-amountRaised
+		playersChecked = 0
+		player.money -= response
+		tableMoney += response
+		userRaised = True
+		playRound()
 
-
-def gameplay():
-	global amountRaised
-	global playersChecked
+def playRound():
 	global playerNumber
-	playersFolded = 0
-	startup()
-	shuffle()
-	dealHand()
-	while True:
-		playersFolded = 0
-		for i in range(playerNumber):
-			ai[i].calcQ(1)
-			ai[i].betCalc()
-			ai[i].bet(i+1)
-			if playersChecked == playerNumber-1:
-				break
-
-			if ai[i].inRound == False:
-				playersFolded += 1
-			if playersFolded >= playerNumber:
-				break
-		if userRaised == True:
-			amountRaised = 0
+	displayCards()
+	for i in range(playerNumber):
+		ai[i].betCalc()
+		ai[i].bet(i)
+	if playersFolded != playerNumber:
 		showOptions()
-		if playersChecked == playerNumber+1:
+	if amountRaised != 0 and userRaised == False:
+		for i in range(playerRaised):
+			ai[i].betCalc()
+			ai[i].bet(i)
+	if playerRaised != 0:
+		for i in range(playerRaised):
+			ai[i].betCalc()
+			ai[i].bet(i)
+	print("*"*60)
+	print("\n"*5)
+
+
+
+def getReadyForNextRound():
+	playersChecked = 0
+	userRaised = False
+	amountRaised = 0
+	print()
+def gameplay():
+	global playersChecked
+	global userRaised
+	global amountRaised
+	global tableMoney
+	while(True):
+		startup()
+		shuffle()
+		dealHand()
+		playRound()
+		getReadyForNextRound()
+		if playersFolded == playerNumber:
+			print("You won this round!")
 			break
-	print('done')
+		deal3()
+		playRound()
+		getReadyForNextRound()
+		if playersFolded == playerNumber:
+			print("You won this round!")
+			break
+		deal1(3)
+		playRound()
+		getReadyForNextRound()
+		if playersFolded == playerNumber:
+			print("You won this round!")
+			break
+		deal1(4)
+		playRound()
+		if playersFolded != playerNumber:
+			winnerlist = [0,0]
+			for i in range(playerNumber):
+				ai[i].calcQ(5)
+			for i in range(playerNumber):
+				if ai[i].handQuality> winnerlist[0]:
+					winnerlist.clear()
+					winnerlist.append(ai[i].handQuality)
+					winnerlist.append(ai[i].pos)
+			player.calcQ()
+			if winnerlist[0]>player.handQuality:
+				print("Player",(winnerlist[1]+1),"won!")
+				ai[winnerlist[1]].money += tableMoney
+				tableMoney = 0
+			else:
+				print("You won this round!")
+				player.hand += tableMoney
+				tableMoney = 0
+		if playersFolded == playerNumber:
+			print("You won this round!")
+		break
+			
+
+
+
 
 gameplay()
 '''
