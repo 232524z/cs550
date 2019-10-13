311aef7f240d511aa126a00bbe124d84f01ff7a7
232524z <ianhaile@me.com>
2019-10-13 13:04:34 -0400

    Update minesweeper.py

--- homework/minesweeper.py
+++ homework/minesweeper.py
@@ -1,21 +1,14 @@
+#Ian
+#OMH
+#10/13/19
+#This program is a text based minesweeper game.
+#Sources: Spencer
+
 import random
 import sys
-#height of board and gutters
-height = int(sys.argv[1])+2
-#width of boeard and gutters
-width = int(sys.argv[2])+2
-#numbering the board does not work if the width of the shown board is greater than 10, so the width of the board with gutters must be less than 12
-if width > 12:
-	while(True):
-		print("Please enter a width between 1 and 10!")
-		try:
-			width = int(input("width: "))+2
-			break
-		except ValueError:
-			pass
-#number of mines
-mines = int(sys.argv[3])
+from os import system, name
 
+#resets all variables so that the game can start
 def reset():
 	global gameover
 	global nonminespaces
@@ -29,30 +22,37 @@ def reset():
 	solboard = [[0 for y in range(width)] for x in range(height)]
 	#board shown to the user
 	showboard = [["▣" for y in range(width)] for x in range(height)]
-	#gets the first move so that the board can be created accordingly
-
+	#prints board and instructions
+	printboard(showboard)
+	print("To reveal a location, type the x coordinate followed by the y coordinate. Ex. 3 4 would reveal 3 accross and 4 down. To flag a location, type the x coordinate, the y coordinate, and the letter f. Ex. 3 4 f would place a flag at 3 across and 4 down")
+	#gets the first move so that the board can be created
 	while True:
-		firstmove=input("move (enter the x coordinate and the y coordinate of the point: ").split()
+		firstmove=input("move: ").split()
 		if len(firstmove)>=2:
 			for i in range(2):
 				firstmove[i] = int(firstmove[i])
 			if firstmove[0]<=width-2 and firstmove[1]<=height-2:
 				break
 
-reset()
-
 def printboard_debug(board):
 	for row in board:
 		print(*row)
 
+#displays a given board to the user
 def printboard(board):
+	#clears the window so that only 1 board is visable
+	clear()
 	#prints numbers at the top of the board
 	print("   ", end=' ')
 	for i in range(1,width-1):
-		print(i, end=' ')
-	print()
+		if i < 10:
+			print(i, end='  ')
+		else:
+			print(i, end=' ')
+			
 	print()
 
+	#prints the board
 	for i in range(1, height-1):
 		#prints numbers on the left of the board
 		if i >= 10:
@@ -60,8 +60,11 @@ def printboard(board):
 		else:
 			print(i, end='   ')
 		#prints each row
-		print(*board[i][1:-1])
+		for j in range(1, width-1):
+			print(board[i][j], end='  ')
+		print()
 
+#adds mines to the board, and increases numbers around the mines
 def addmines():
 	for i in range(mines):
 		#selects a roandom location for the mine
@@ -77,6 +80,7 @@ def addmines():
 		for y in range(-1, 2):
 			for x in range(-1,2):
 				addnumber(row+y,col+x)
+	#sets the gutters to 5 so that when zeros are revealed, the gutters aren't affected
 	for i in range(width):
 		solboard[0][i] = 5
 		solboard[height-1][i]= 5
@@ -89,12 +93,15 @@ def addnumber(row, col):
 	if solboard[row][col] != "*":
 		solboard[row][col] +=1
 
+#reveals a given location on the board
 def reveal(x,y):
 	global nonminespaces
 	global gameover
-	#sets the position on the board the players sees to the number or bomb on the solution board
+	#makes sure the location is not already revealed, and is not a gutter
 	if showboard[y][x] == "▣" and y>= 1 and y<=height-2 and x>=1 and x<=width-2:
+		#puts the number on the solution board into the show board
 		showboard[y][x] = solboard[y][x]
+		#if the number was a zero, reveals zeros around the coordinate
 		if showboard[y][x] == 0:
 			revealzeros(x,y)
 		#if the player revealed a mine, the game ends
@@ -109,22 +116,25 @@ def reveal(x,y):
 				print("\nYou won!")
 				gameover = True
 
+#goes through all surounding places and reveals the numbers
 def revealzeros(x,y):
 	for i in range (-1, 2):
 		for z in range(-1, 2):
 			reveal(x+z,y+i)
 
-
+#puts a flag at a given location
 def flag(x,y):
 	global gameover
-	#if the position is not a flag, puts a flag there
-	if showboard[y][x] != "⚐":
+	#if the position is not yet revealed or flagged, places a flag
+	if showboard[y][x] == "▣":
 		showboard[y][x] = "⚐"
-	else:
+	elif showboard[y][x] == "⚐":
 		#if the position is a flag, put a box there
 		showboard[y][x] = "▣"
 
+#gets the user's action and executes the appropriet function
 def action():
+	#takes the input and checks for errors
 	while True:
 		action=input("move (x,y,(f)): ").split()
 		if len(action)>=2:
@@ -133,20 +143,28 @@ def action():
 			if action[0]<=width-2 and action[1]<=height-2:
 				break
 
-
-	#if there are just coordinates
+	#if there are just coordinates (no flag)
 	if len(action) == 2:
 		reveal(action[0],action[1])
 	#if there is also a flag
 	elif len(action) == 3:
 		flag(action[0],action[1])
 
+#when the game ends, prints the solution, resets, and offers to play again
 def gameOver():
 	printboard(solboard)
 	playagain = input("Play again?\n").strip().lower()
 	if playagain[0] == "y":
 		reset()
 		gameplay()
+	else:
+		quit()
+#clears the terminal. The clear command differs based on operation system, hence the if statemence
+def clear():
+	if name == "nt":
+		_ = system("cls")
+	else:
+		_ = system("clear")
 
 def gameplay():
 	global showboard
@@ -155,15 +173,65 @@ def gameplay():
 	addmines()
 	#reveals the first move
 	reveal(firstmove[0],firstmove[1])
-	print("To reveal a location, type the x coordinate followed by the y coordinate. Ex. 3 4 would reveal 3 accross and 4 down. To flag a location, type the x coordinate, the y coordinate, and the letter f. Ex. 3 4 f would place a flag at 3 across and 4 down")
+	#checks if the game has ended
 	if gameover == True:
 		gameOver()
 		print("\n\n")
+	print()
 	while True:
+		#shows the board
 		printboard(showboard)
+		#gets an action
 		action()
+		#checks if the game is over
 		if gameover == True:
 			gameOver()
 			break
 		print("\n\n")
+
+#sets the recursion limit to 20,000 so that the code can be much more concise by using recursion
+sys.setrecursionlimit(20000)
+
+#height of board and gutters
+height = int(sys.argv[1])+2
+
+#width of boeard and gutters
+width = int(sys.argv[2])+2
+
+#number of mines
+mines = int(sys.argv[3])
+
+#numbering the board does not work if the width or height of the shown board is greater than 99, and the game could crash if the board is too large, so it is limited to a 99x99 board
+#makes sure the width is between 1 and 99
+if width > 101 or width<1:
+	while(True):
+		print("Please enter a width between 1 and 99!")
+		try:
+			width = int(input("width: "))+2
+			if width<100 and width > 0:
+				break
+		except ValueError:
+			pass
+if height > 101 or height<1:
+	while(True):
+		print("Please enter a height between 1 and 99!")
+		try:
+			height = int(input("height: "))+2
+			if height<100 and height > 0:
+				break
+		except ValueError:
+			pass
+
+#makes sure there is at least 1 bomb, and no more than width*height bombs (maximum possible)
+if mines > (width-2)*(height-2) or mines<1:
+	while(True):
+		print("Please enter mines between 1 and",((width-2)*(height-2)),"!")
+		try:
+			mines = int(input("mines: "))
+			if mines<((width-2)*(height-2)) and mines > 0:
+				break
+		except ValueError:
+			pass
+
+reset()
 gameplay()
